d6366d47aa02957aedf8ef3309343fedb0487203
Andro87 <ggandolfo987@gmail.com>
2021-01-14 10:03:17 +0100

    [Change] -change

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const config = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+                test: /\.(js|ts)x?$/
+            },
+            use: ["@svgr/webpack"]
+        });
+
+        return config;
+    }
+};
 module.exports = {
     ...config,
     i18n: {
